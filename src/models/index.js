37fa967dcc566f98be9f1e8cd0f72f4630a931bc
MendritMorina <mendritmorina4@gmail.com>
2022-04-22 15:45:49 +0200

    Mergin with Ermir

--- src/models/index.js
+++ src/models/index.js
@@ -4,9 +4,25 @@ const Recommendation = require('./Recommendation');
 const RecommendationCard = require('./RecommendationCard');
 const Subscription = require('./Subscription');
 const SubscriptionType = require('./SubscriptionType');
+const Admin = require('./Admin');
+const Company = require('./Company');
+const Coupon = require('./Coupon');
+const Story = require('./Story');
+const UsedCoupon = require('./UsedCoupon');
 
 // Bundler object that is used to export all models inside ./src/models.
-const bundler = { User, Recommendation, RecommendationCard, Subscription, SubscriptionType };
+const bundler = {
+  User,
+  Recommendation,
+  RecommendationCard,
+  Subscription,
+  SubscriptionType,
+  Admin,
+  Company,
+  Coupon,
+  Story,
+  UsedCoupon,
+};
 
 // Exports of this file.
 module.exports = bundler;
