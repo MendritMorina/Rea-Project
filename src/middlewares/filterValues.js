b68b83985b1dd029aef99c8513f12eb265b4d4d8
Ermir Milaimi <ermir-milaimi@example.com>
2022-04-28 12:54:44 +0200

    Changed the strucutre of middleware filterValues

--- src/middlewares/filterValues.js
+++ src/middlewares/filterValues.js
@@ -4,23 +4,14 @@ const filterValuesFromSelect = (select, removeValues) => {
 
   const newArraySelect = removeDuplicates(arraySelect);
 
-  for (let i = 0; i < newArraySelect.length; i++) {
-    if (notContainsValue(newArraySelect[i], removeValues)) {
-      newArray.push(newArraySelect[i]);
+  newArraySelect.forEach((selectValue) => {
+    if (!removeValues.includes(selectValue)) {
+      newArray.push(selectValue);
     }
-  }
+  });
 
   function removeDuplicates(arr) {
     return arr.filter((item, index) => arr.indexOf(item) === index);
-  }
-
-  function notContainsValue(selectValue, removeValues) {
-    for (let i = 0; i < removeValues.length; i++) {
-      if (selectValue === removeValues[i]) {
-        return false;
-      }
-    }
-    return true;
   }
 
   return newArray.join(',');
