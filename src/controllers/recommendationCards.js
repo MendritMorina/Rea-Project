4e1829439e2c5a65276736bcdfc1ab089eb9b41f
Ermir Milaimi <ermir-milaimi@example.com>
2022-04-29 16:23:57 +0200

    Added file upload in create RecommendationCard

--- src/controllers/recommendationCards.js
+++ src/controllers/recommendationCards.js
@@ -1,6 +1,10 @@
+// Imports: core node modules.
+const fs = require('fs');
+const path = require('path');
+
+// Imports: local files.
 const RecommendationCard = require('../models/RecommendationCard');
 const Recommendation = require('../models/Recommendation');
-
 const { asyncHandler } = require('../middlewares');
 
 const { ApiError } = require('../utils/classes');
@@ -64,7 +68,7 @@ const getOne = asyncHandler(async (request, response) => {
 
 /**
  * @description Create a recommendationCard.
- * @route       POST /api/recommendationcards/.
+ * @route       POST /api/recommendationcards.
  * @access      Private.
  */
 const create = asyncHandler(async (request, response, next) => {
@@ -100,9 +104,65 @@ const create = asyncHandler(async (request, response, next) => {
     }
   );
 
-  return response
-    .status(httpCodes.CREATED)
-    .json({ success: true, data: { recommendationCard, updatedRecommendation }, error: null });
+  let smallResult = null;
+  let mediumResult = null;
+  let largeResult = null;
+  let thumbnailResult = null;
+
+  if (request.files && Object.keys(request.files).length && request.files['small']) {
+    smallResult = await uploadFile(recommendationCard._id, userId, request, 'small');
+    if (!smallResult.success) {
+      next(new ApiError(smallResult.error, httpCodes.INTERNAL_ERROR));
+      return;
+    }
+  }
+
+  if (request.files && Object.keys(request.files).length && request.files['medium']) {
+    mediumResult = await uploadFile(recommendationCard._id, userId, request, 'medium');
+    if (!mediumResult.success) {
+      next(new ApiError(mediumResult.error, httpCodes.INTERNAL_ERROR));
+      return;
+    }
+  }
+
+  if (request.files && Object.keys(request.files).length && request.files['large']) {
+    largeResult = await uploadFile(recommendationCard._id, userId, request, 'large');
+    if (!largeResult.success) {
+      next(new ApiError(largeResult.error, httpCodes.INTERNAL_ERROR));
+      return;
+    }
+  }
+
+  if (request.files && Object.keys(request.files).length && request.files['thumbnail']) {
+    thumbnailResult = await uploadFile(recommendationCard._id, userId, request, 'thumbnail');
+    if (!thumbnailResult.success) {
+      next(new ApiError(thumbnailResult.error, httpCodes.INTERNAL_ERROR));
+      return;
+    }
+  }
+
+  // let updatedRecommendationCard = attachFileResultToRecommendationCard(
+  //   smallResult,
+  //   mediumResult,
+  //   largeResult,
+  //   thumbnailResult,
+  //   recommendationCard
+  // );
+
+  const updatedRecommendationCard =
+    (smallResult && smallResult.success) ||
+    (mediumResult && mediumResult.success) ||
+    (largeResult && largeResult.success) ||
+    (thumbnailResult && thumbnailResult.success)
+      ? await RecommendationCard.findOne({ _id: recommendationCard._id })
+      : {};
+
+  return response.status(httpCodes.CREATED).json({
+    success: true,
+    //data: { updatedRecommendation, updatedRecommendationCard },
+    data: { updatedRecommendation, updatedRecommendationCard },
+    error: null,
+  });
 });
 
 /**
@@ -238,6 +298,120 @@ const deleteOne = asyncHandler(async (request, response, next) => {
     .status(httpCodes.OK)
     .json({ success: true, data: { recommendationCard: deletedRecommendationCard }, error: null });
 });
+
+const attachFileResultToRecommendationCard = (
+  smallResult,
+  mediumResult,
+  largeResult,
+  thumbnailResult,
+  recommendationCard
+) => {
+  let updatedRecommendationCard = null;
+
+  if (smallResult && smallResult.success) {
+    updatedRecommendationCard =
+      smallResult && smallResult.success && smallResult.data
+        ? smallResult.data.updatedRecommendationCard
+        : recommendationCard;
+  } else if (mediumResult && mediumResult.success) {
+    updatedRecommendationCard =
+      mediumResult && mediumResult.success && mediumResult.data
+        ? mediumResult.data.updatedRecommendationCard
+        : recommendationCard;
+  } else if (largeResult && largeResult.success) {
+    updatedRecommendationCard =
+      largeResult && largeResult.success && largeResult.data
+        ? largeResult.data.updatedRecommendationCard
+        : recommendationCard;
+  } else if (thumbnailResult && thumbnailResult.success) {
+    updatedRecommendationCard =
+      thumbnailResult && thumbnailResult.success && thumbnailResult.data
+        ? thumbnailResult.data.updatedRecommendationCard
+        : recommendationCard;
+  } else {
+    updatedRecommendationCard = recommendationCard;
+  }
+
+  return updatedRecommendationCard;
+};
+
+const uploadFile = async (recommendationCardId, userId, request, fileType) => {
+  if (!request.files[fileType]) {
+    return { success: false, data: null, error: `File name must be ${fileType}`, code: httpCodes.BAD_REQUEST };
+  }
+
+  const allowedFileTypes = ['small', 'medium', 'large', 'thumbnail'];
+
+  if (!allowedFileTypes.includes(fileType)) {
+    return {
+      success: false,
+      data: null,
+      error: `File Type must be of ${allowedFileTypes}`,
+      code: httpCodes.BAD_REQUEST,
+    };
+  }
+
+  const { data, mimetype, name, size } = request.files[fileType];
+
+  const type = mimetype.split('/').pop();
+
+  let allowedTypes = ['jpeg', 'jpg', 'png'];
+
+  if (!allowedTypes.includes(type)) {
+    return { success: false, data: null, error: `Wrong ${fileType} type!`, code: httpCodes.BAD_REQUEST };
+  }
+
+  const recommendationCard = await RecommendationCard.findOne({ _id: recommendationCardId });
+  if (!recommendationCard) {
+    return {
+      success: false,
+      data: null,
+      error: 'RecommendationCard not found!',
+      code: httpCodes.INTERNAL_ERROR,
+    };
+  }
+
+  if (recommendationCard[fileType] && recommendationCard[fileType].name === name) {
+    return { success: true, data: { updatedRecommendationCard: recommendationCard }, error: null, code: null };
+  }
+
+  const fileName = `${recommendationCard._id}_${fileType}_${Date.now()}.${type}`;
+  const filePath = path.join(__dirname, `../../public/recommendationcards/${fileName}`);
+
+  try {
+    fs.writeFileSync(filePath, data, { encoding: 'utf-8' });
+  } catch (error) {
+    return { success: false, data: null, error: `Failed to upload ${fileType}!`, code: httpCodes.INTERNAL_ERROR };
+  }
+
+  // const publicURL = isMode('production') ? process.env.PUBLIC_PROD_URL : process.env.PUBLIC_DEV_URL;
+  const publicURL = 'http://localhost:5000';
+  const fileURL = `${publicURL}/recommendationcards/${fileName}`;
+
+  const updatedRecommendationCard = await RecommendationCard.findOneAndUpdate(
+    { _id: recommendationCard._id },
+    {
+      $set: {
+        [fileType]: {
+          url: fileURL,
+          name: name,
+          mimetype: mimetype,
+          size: size,
+        },
+        lastEditBy: userId,
+        lastEditAt: new Date(Date.now()),
+      },
+    },
+    { new: true }
+  );
+  if (!updatedRecommendationCard) {
+    return { success: false, data: null, error: `Failed to upload ${fileType}!`, code: httpCodes.INTERNAL_ERROR };
+  }
+
+  console.log(updatedRecommendationCard);
+
+  return { success: true, data: { updatedRecommendationCard }, error: null, code: null };
+};
 
 // Exports of this file.
 module.exports = { getAll, getOne, create, deleteOne, updateOne };
