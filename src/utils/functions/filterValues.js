e221e49367cdae82df24e4769c02ab18e0438322
Lum Xhakalija <lumi.xhakalija@gmail.com>
2022-06-03 11:10:13 +0200

    follow conventions in functions

--- src/utils/functions/filterValues.js
+++ src/utils/functions/filterValues.js
@@ -1,3 +1,4 @@
+// Function that is used to create projection query for mongoose.
 const filterValuesFromSelect = (select, removeValues) => {
   const arraySelect = select.split(',');
   const newArray = [];
@@ -5,12 +6,11 @@ const filterValuesFromSelect = (select, removeValues) => {
   const newArraySelect = arraySelect.filter((item, index) => arraySelect.indexOf(item) === index);
 
   newArraySelect.forEach((selectValue) => {
-    if (!removeValues.includes(selectValue)) {
-      newArray.push(selectValue);
-    }
+    if (!removeValues.includes(selectValue)) newArray.push(selectValue);
   });
 
   return newArray.join(' ');
 };
 
+// Exports of this file.
 module.exports = filterValuesFromSelect;
